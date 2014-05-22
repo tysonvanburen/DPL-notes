afe62cda14e978c45e83db753a977bf5eece0893
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-05-22 15:09:42 -0600

    added new ruby code

--- ruby.rb
+++ ruby.rb
@@ -2,5 +2,9 @@ puts "What is your first name?"
 first_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
+puts "Where are you from?"
+home_town = gets.chomp
+puts "Aren't there a lot of mornmans in Provo?"
+dumb_question = gets.chomp
 
-puts "Hello,  #{first_name} #{last_name}."
+puts "Hello,  #{first_name} #{last_name}! Who lives with all the Mormans ;)"
