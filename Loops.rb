8a357940a050f99988e2ad130151de95bdf94a7a
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-02 10:08:21 -0600

    modified Loops.rb

--- Loops.rb
+++ Loops.rb
@@ -21,3 +21,27 @@ unless sun_is_up
 else
    puts "it's day time"
 end
+
+-------------------------------
+
+i = 0
+string = "Hello World"
+
+while i <= 10
+  sting = "Hello world #{i}"
+  puts "i is now #{i}"
+  i += 1
+end
+
+puts "The final value of i is #{i}"
+puts "The final value of string is #{string}"
+
+
+-------------------------------
+
+i = 0
+
+until i > 10 do
+  puts "i is now #{i}"
+  i += 1
+end
